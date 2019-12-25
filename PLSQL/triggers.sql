2f864ff01838c7cd84821a7ad873c258dec3d6ff
KritikaChoudhary <kritikac2410@gmail.com>
2019-12-25 22:00:49 +0530

    Delete triggers.sql

--- PLSQL/triggers.sql
+++
@@ -1,7 +0,0 @@
-create or replace trigger noEnter after delete or insert or update on employee
-  begin
-  if (To_char(sysdate,'day') in ('Sat','Sun')) then
-  raise_application_error('-20500,Permission Denied');
-  end if;
-  end;
-  /
